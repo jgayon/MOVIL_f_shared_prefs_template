8153379e45b7a4c4a02d89c19137252b13d51e33
jgayon <79709433+jgayon@users.noreply.github.com>
2025-03-26 10:00:09 -0500

    Update auth_repo.dart

--- lib/data/repositories/auth_repo.dart
+++ lib/data/repositories/auth_repo.dart
@@ -10,30 +10,40 @@ class AuthRepo implements IAuthRepo {
   @override
   Future<User> getUserFromEmail(email) async {
     //TODO: implement getUserFromEmail
+    try {
+      return await localAuthSource.getUserFromEmail(email);
+    } catch (e) {
+      throw Exception("Error obtaining user: $e");
+    }
   }
 
   @override
   Future<void> logout() async {
     //TODO: implement logout
+   localAuthSource.logout;
   }
 
   @override
   Future<void> signup(email, password) async {
     //TODO: implement signup
+    await localAuthSource.signup(email, password);
   }
 
   @override
   Future<String> getLoggedUser() async {
     //TODO: implement getLoggedUser
+    return await localAuthSource.getLoggedUser();
   }
 
   @override
   Future<bool> isLogged() async {
     //TODO: implement isLogged
+    return await localAuthSource.isLogged();
   }
 
   @override
   Future<void> setLoggedIn() async {
     //TODO: implement setLoggedIn
+    await localAuthSource.setLoggedIn();
   }
 }
