ef9693b2a90c1b8bc2326255a4c9f8c2545ed3ed
jgayon <79709433+jgayon@users.noreply.github.com>
2025-03-26 10:00:49 -0500

    Update shared_pref_local_auth_source.dart

--- lib/data/datasources/shared_prefs/shared_pref_local_auth_source.dart
+++ lib/data/datasources/shared_prefs/shared_pref_local_auth_source.dart
@@ -19,20 +19,32 @@ class SharedPrefLocalAuthSource implements ILocalAuthSource {
   Future<User> getUserFromEmail(email) async {
     //TODO: implement getUserFromEmail, return a User object
     // if no user is found, throw "User not found"
+    String? storedEmail = await _sharedPreferences.retrieveData<String>('user');
+    String? storedPassword = await _sharedPreferences.retrieveData<String>('password');
+    if (storedEmail == email) {
+        return User(email: storedEmail!, password: storedPassword! );
+    } else {
+      throw Exception("User not found");
+    }
   }
 
   @override
   Future<bool> isLogged() async {
     //TODO: implement isLogged, return a boolean, default is false
+    return await _sharedPreferences.retrieveData<bool>('logged') ?? false;
   }
 
   @override
   Future<void> signup(email, password) async {
     //TODO: implement signup, stroe the email and pass on shared preferences
+    await _sharedPreferences.storeData('user', email);
+    await _sharedPreferences.storeData('password', password);
+    await setLoggedIn();
   }
 
   @override
   Future<void> setLoggedIn() async {
     //TODO: implement setLoggedIn, store a boolean on shared preferences
+    await _sharedPreferences.storeData('logged', true);
   }
 }
